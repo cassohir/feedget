1f93438780b1efcee3937b4074cf3ac76c2d5029
cassohir <cassio.hir@gmail.com>
2022-05-04 12:03:37 -0300

    Inicizalizando aba de comentário com tailwind

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,5 +1,6 @@
 import {ChatTeardropDots} from 'phosphor-react';
 import {Popover} from '@headlessui/react';
+import {WidgetForm} from './WidgetForm';
 // import { useState } from 'react';
 
 //Estado 
@@ -11,10 +12,14 @@ export function Widget(){
   //   setIsWidgetOpen(!isWidgetOpen);
   // }
   return (  
-    <Popover className="absolute bottom-5 right-4">
+    <Popover className="absolute bottom-4 right-4 md:botton-8 md:right-10 flex flex-col items-end  ">
       
       
-      <Popover.Panel>Hello World</Popover.Panel>
+      <Popover.Panel>
+
+        <WidgetForm />
+
+      </Popover.Panel>
 
       <Popover.Button className="bg-primary-200 rounded-full px-3 h-12 text-primary-100 flex items-center group">
         <ChatTeardropDots weight="bold" className="w-6 h-6"/>
